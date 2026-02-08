2700b6a69212cd655f1a6a52ba814d4519e20844
Nikolay Gakman <gakmannn@ya.ru>
2026-02-08 21:21:13 +0300

    posts

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,9 @@
 <template>
+  <nav style="display:flex; gap:10px">
+    <NuxtLink to="/">home</NuxtLink>
+    <NuxtLink to="/users">users</NuxtLink>
+    <NuxtLink to="/posts">posts</NuxtLink>
+  </nav>
   <div>
     <NuxtPage />
   </div>
